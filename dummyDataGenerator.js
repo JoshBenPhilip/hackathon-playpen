927fcb17a5bd97000e7280a971e750219c361507
JoshBenPhilip <joshben4c@gmail.com>
2022-10-08 22:58:21 -0400

    working

---
+++ dummyDataGenerator.js
@@ -0,0 +1,26 @@
+data.map(({ scoreValue, coordsValue }) => {
+  return {
+    score: scoreValue,
+    coords: coordsValue,
+  };
+});
+
+// }, {
+//   score: 19191990,
+//   coords: [-79.91746, 40.44356],
+// }}
+const a = Math.random;
+const b = Math.random;
+const c = Math.random;
+
+const array = [];
+function createDummyData(a, b) {
+  array.push({
+    score: a;
+    coords: [b, c];
+  })
+}
+
+
+for (let i = 0; i<10;  
+console
